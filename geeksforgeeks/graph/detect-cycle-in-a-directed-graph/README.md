2f26a17ca69102c575554932f3ce29d1b01f9cbf
Mostafa Asgari <mostafa.asg@gmail.com>
2020-12-13 23:25:07 +0330

    Detect cycle in a directed graph

---
+++ geeksforgeeks/graph/detect-cycle-in-a-directed-graph/README.md
@@ -0,0 +1,9 @@
+Given a Directed Graph. Check whether it contains any cycle or not.
+
+**Input**: The first line of the input contains an integer 'T' denoting the number of test cases. Then 'T' test cases follow. Each test case consists of two lines. Description of testcases is as follows: The First line of each test case contains two integers 'N' and 'M'  which denotes the no of vertices and no of edges respectively. The Second line of each test case contains 'M'  space separated pairs u and v denoting that there is an uni-directed  edge from u to v .
+
+**Output**:
+The method should return 1 if there is a cycle else it should return 0.
+
+## Original link
+https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
---
+++ geeksforgeeks/graph/detect-cycle-in-a-directed-graph/solution1.py
@@ -0,0 +1,65 @@
+# Your task is to complete this function
+# Function should return True/False or 1/0
+# Graph(graph) is a defaultict of type List
+# n is no of Vertices
+def isCyclic(n, graph):
+    # Code here
+    detector = CycleDetector(graph)
+    return detector.isCyclic()
+
+
+class CycleDetector():
+
+    def __init__(self, graph: dict):
+        self.graph = graph
+
+    def isCyclic(self):
+        vertices = set(graph.keys())
+
+        for vertex in vertices:
+            seen_vertices = []
+            
+            result = self._expand(vertex, seen_vertices)
+            if result == True:
+                return True
+
+        return False
+
+    def _expand(self, vertex, seen_vertices):
+        seen_vertices.append(vertex)
+
+        for v in self.graph[vertex]:
+            if v in seen_vertices:
+                return True
+            else:
+                result = self._expand(v, seen_vertices)
+                if result == True:
+                    return True
+            seen_vertices.pop()
+
+        return False
+
+#{ 
+#  Driver Code Starts
+
+from collections import defaultdict
+
+def creategraph(n, arr, graph):
+    i = 0
+    while i < 2 * e:
+        graph[arr[i]].append(arr[i + 1])
+        # graph[arr[i + 1]].append(arr[i])
+        i += 2
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n,e = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        graph = defaultdict(list)
+        creategraph(e, arr, graph)
+        if isCyclic(n, graph):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
